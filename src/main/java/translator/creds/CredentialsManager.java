6afd831e9323ee69a1427e920b3ccc07a9e68108
ikarthikb <karthik.uh@gmail.com>
2016-03-21 14:46:05 -0500

    Cleaned creds

--- src/main/java/translator/creds/CredentialsManager.java
+++ src/main/java/translator/creds/CredentialsManager.java
@@ -1,35 +1,32 @@
 package translator.creds;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-
 /**
  * Created by Karthik on 3/21/16.
  */
 public class CredentialsManager {
 
     public static String getAWSAccessKey(){
-        return "AKIAIED74ANCXMLNU4IA";
+        return "YOUR_AWS_ACCESS_KEY";
     }
 
     public static String getAWSSecretKey(){
-        return "qLyhXIVxGskt0+npOfa7Fpb3nyUT3ql1vazzOYEY";
+        return "YOUR_AWS_SECRET_KEY";
     }
 
     public static String getIvonaAccessKey(){
-        return "GDNAILY2CRF6WUYPOF2Q";
+        return "YOUR_AWS_ACCESS_KEY";
     }
 
     public static String getIvonaSecretKey(){
-        return "xlafUwo7VpIKLmqBSP+QTaz8hlXZVUgG3tsnWmJc";
+        return "YOUR_AWS_SECRET_KEY";
     }
 
     public static String getGoogleApiKey(){
-        return "AIzaSyBFG_VxY35RMpwM1Jqxyuu5ai6_uT_SRaY";
+        return "YOUR_GOOGLE_API_KEY";
     }
 
     public static String getVoiceRssApiKey(){
-        return "d87150427f7149d6a2e32f111ebe68c";
+        return "YOUR_VOICE_KEY";
     }
 
 
